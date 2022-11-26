06f2efd05043f8c3a7e946a25f44183eede35913
John Gerald Agbayani <agbayani.johngerald@gmail.com>
2022-11-26 13:28:28 +0800

    Add body-parser and mysql connector

--- index.ts
+++ index.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import bodyParser from 'body-parser'
 
 const app = express();
 const port = 3000;
 
-app.listen(port, () => {
+app.use(bodyParser.json());
+
+app.listen(port, async () => {
   console.log(`[server]: Server is running at https://localhost:${port}`);
 });
