41ab466d22173f90b544b157fb6c50123f7139e3
John Gerald Agbayani <agbayani.johngerald@gmail.com>
2022-11-26 15:55:53 +0800

    Create database connection

--- index.ts
+++ index.ts
@@ -1,15 +1,28 @@
 import express from "express";
-import bodyParser from 'body-parser'
+import bodyParser from "body-parser";
+import mysql, { Connection } from "mysql2/promise";
 
 const app = express();
 const port = 3000;
 
+let connection: Connection
+
 app.use(bodyParser.json());
 
 app.get("/healthcheck", (req, res) => {
-  res.send("Hello, world!")
-})
+  res.send("Hello, world!");
+});
 
 app.listen(port, async () => {
+  connection = await mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Password@123',
+    database: 'cc_106',
+    port: 3306,
+  })
+
+connection.connect()
+
   console.log(`[server]: Server is running at https://localhost:${port}`);
 });
