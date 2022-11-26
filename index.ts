a37c42f4e095cdbecb6d54ae4acf2ff99bed1518
John Gerald Agbayani <agbayani.johngerald@gmail.com>
2022-11-26 17:07:55 +0800

    Create POST /users endpoint

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ import mysql, { Connection } from "mysql2/promise";
 const app = express();
 const port = 3000;
 
-let connection: Connection
+let connection: Connection;
 
 app.use(bodyParser.json());
 
@@ -13,23 +13,40 @@ app.get("/healthcheck", (req, res) => {
   res.send("Hello, world!");
 });
 
-app.get('/users', async (req, res) => {
-  const result = await connection.query("SELECT * FROM users")
-  const users = result[0]
+app.get("/users", async (req, res) => {
+  const result = await connection.query("SELECT * FROM users");
+  const users = result[0];
 
-  res.send(users)
-})
+  res.send(users);
+});
+
+app.post("/users", async (req, res) => {
+  const request_body = req.body;
+
+  const user_first_name = request_body.first_name;
+  const user_last_name = request_body.last_name;
+  const user_age = request_body.age;
+  const user_sex = request_body.sex;
+  const user_is_employed = request_body.is_employed;
+
+  connection.query(
+    "INSERT INTO users (first_name, last_name, age, sex, is_employed) VALUES (?, ?, ?, ?, ?);",
+    [user_first_name, user_last_name, user_age, user_sex, user_is_employed]
+  );
+
+  res.status(201).send();
+});
 
 app.listen(port, async () => {
   connection = await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Password@123',
-    database: 'cc_106',
+    host: "localhost",
+    user: "root",
+    password: "Password@123",
+    database: "cc_106",
     port: 3306,
-  })
+  });
 
-connection.connect()
+  connection.connect();
 
   console.log(`[server]: Server is running at https://localhost:${port}`);
 });
