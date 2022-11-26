d475889c9aec82be97c1b54111cf5b93ffa4ce41
John Gerald Agbayani <agbayani.johngerald@gmail.com>
2022-11-26 15:41:16 +0800

    Add healthcheck

--- index.ts
+++ index.ts
@@ -6,6 +6,10 @@ const port = 3000;
 
 app.use(bodyParser.json());
 
+app.get("/healthcheck", (req, res) => {
+  res.send("Hello, world!")
+})
+
 app.listen(port, async () => {
   console.log(`[server]: Server is running at https://localhost:${port}`);
 });
